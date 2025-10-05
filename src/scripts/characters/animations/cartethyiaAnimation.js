09b8f26c1156903a39986e9d0be7cb6e3947178f
Kaguyo <gabrieltroop27@gmail.com>
2025-10-05 15:58:49 -0300

    preparing master branch

--- src/scripts/characters/animations/cartethyiaAnimation.js
+++
@@ -1,5 +0,0 @@
-class CartethyiaAnimation {
-    static GetGohanSpritesObj() {
-
-    }
-}
--- src/scripts/hud/characterCard.js
+++
@@ -1,9 +0,0 @@
-const CardIcon1 = new Image();
-CardIcon1.src = "../../assets/hud/characterContainer/cards/CardIcon.png"
-
-
-class CharacterCard {
-    static drawCard(ctx, dx, dy) {
-        ctx.drawImage(CardIcon1, dx, dy);
-    }
-}

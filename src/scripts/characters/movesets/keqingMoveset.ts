8f77f23fbbada977c897b18d75eae014b6018f86
Kaguyo <gabrieltroop27@gmail.com>
2025-11-02 02:22:12 -0300

    1

--- src/scripts/characters/movesets/keqingMoveset.ts
+++
@@ -1,7 +0,0 @@
-import { GameProperties } from "../../options/gameProperties.js";
-import { KeqingAnimation } from "../animations/keqingAnimation.js";
-
-export class KeqingMoveset {
-     // public methods region
-
-}
